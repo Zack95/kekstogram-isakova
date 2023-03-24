649c4582682e3557ac43a5b07d267a5db800e779
Ilia Kovalenko <ilia.kovalenko@pressfire.games>
2023-03-25 02:43:46 +0300

    add reset form on close

--- js/uploadImage.js
+++ js/uploadImage.js
@@ -1,4 +1,4 @@
-import { isEscapeKey } from './util.js';
+import { imageUploadForm, pristine } from "./validation.js";
 
 const uploadPhotoSection = document.querySelector('.img-upload__overlay');
 const uploadTemplate = document.querySelector('#upload-file');
@@ -9,17 +9,18 @@ uploadTemplate.onchange = function () {
   document.body.classList.add('modal-open');
 };
 
-
 const closephotoForm = () => {
-  uploadPhotoSection.classList.add('hidden');
-  document.body.classList.remove('modal-open');
+  imageUploadForm.reset();
+  pristine.reset();
+  uploadPhotoSection.classList.add("hidden");
+  document.body.classList.remove("modal-open");
 };
 
-cancelButton.addEventListener('click', () => {
+cancelButton.addEventListener("click", () => {
   closephotoForm();
 });
 
-cancelButton.addEventListener('keydown', (evt) => {
+cancelButton.addEventListener("keydown", (evt) => {
   if (isEscapeKey(evt)) {
     closephotoForm();
   }
