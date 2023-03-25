701a987ecea9008948f1badbc3830b11b3022b0b
Ilia Kovalenko <ilia.kovalenko@pressfire.games>
2023-03-25 13:28:08 +0300

    refact close image redactor

--- js/uploadImage.js
+++ js/uploadImage.js
@@ -12,26 +12,32 @@ const imageUploadOverlay = document.querySelector(".img-upload__overlay");
 const imageUpload = document.querySelector("#upload-file");
 const imageUploadCancel = document.querySelector("#upload-cancel");
 
+function closeImageRedactor() {
+  imageUploadForm.reset();
+  pristine.reset();
+  imageUploadOverlay.classList.add("hidden");
+  document.body.classList.remove("modal-open");
+
+  scaleImageReset();
+  effectReset();
+
+  imageUploadCancel.removeEventListener("click", closeImageRedactor);
+}
+
 function openImageRedactor() {
   imageUploadOverlay.classList.remove("hidden");
   scaleValueControl.value = `${scaleDefault}%`;
   document.body.classList.add("modal-open");
-  imageUploadForm.addEventListener("change", effectChange);
-}
 
-function closeImageRedactor() {
-  imageUploadForm.reset();
-  pristine.reset();
-  imagePreview.style.transform = `scale(${scaleDefault / 100})`;
-  imageUploadOverlay.classList.add("hidden");
-  document.body.classList.remove("modal-open");
+  imageUploadForm.addEventListener("change", effectChange);
+  imageUploadCancel.addEventListener("click", closeImageRedactor);
 }
 
 imageUpload.addEventListener("change", openImageRedactor);
 
-imageUploadCancel.addEventListener("click", () => {
-  closeImageRedactor();
-});
+//imageUploadCancel.addEventListener("click", () => {
+//  closeImageRedactor();
+//});
 
 imageUploadCancel.addEventListener("keydown", (evt) => {
   if (isEscapeKey(evt)) {
