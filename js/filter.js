7646f01ded9147b1f4cc49073e63cc47fffd70fc
Ilia Kovalenko <ilia.kovalenko@pressfire.games>
2023-03-25 13:25:31 +0300

    add nor work slider

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,15 @@
 import { imagePreview } from "./scaleImage.js";
-const sliderElement = document.querySelector(".effect-level__slider");
-const sliderResult = document.querySelector(".effect-level__value");
-
+const imgPreviewContainer = document.querySelector(
+  ".img-upload__preview-container"
+);
+const sliderElement = imgPreviewContainer.querySelector(
+  ".effect-level__slider"
+);
+const sliderContainer = imgPreviewContainer.querySelector(
+  ".img-upload__effect-level"
+);
+const valueElement = imgPreviewContainer.querySelector(".effect-level__value");
+//const sliderResult = document.querySelector(".effect-level__value");
 const imageEffect = [
   { name: "none", filter: "none", min: 0, max: 100, step: 1 },
   { name: "chrome", filter: "grayscale", min: 0, max: 1, step: 0.1, unit: "" },
@@ -11,11 +19,70 @@ const imageEffect = [
   { name: "heat", filter: "brightness", min: 1, max: 3, step: 0.1, unit: "" },
 ];
 
+export const imageEffectNone = imageEffect[0];
+export let imageEffectCurrent = imageEffectNone;
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: imageEffectNone.min,
+    max: imageEffectNone.max,
+  },
+  start: imageEffectNone.max,
+  step: imageEffectNone.step,
+  connect: "lower",
+});
+
+const imageEffectOriginalCheck = () => {
+  if (imageEffectCurrent === imageEffectNone) {
+    sliderContainer.classList.add("hidden");
+    imagePreview.removeAttribute("class");
+    //imagePreview.className = "";
+    imagePreview.style.removeProperty("filter");
+  } else {
+    sliderContainer.classList.remove("hidden");
+  }
+};
+
+const effectSliderUpdate = () => {
+  imageEffectOriginalCheck();
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: imageEffectCurrent.min,
+      max: imageEffectCurrent.max,
+    },
+    step: imageEffectCurrent.step,
+    start: imageEffectCurrent.max,
+  });
+};
+
+export const effectReset = () => {
+  imageEffectCurrent = imageEffectNone;
+  effectSliderUpdate();
+};
+
 export function effectChange(evt) {
   if (evt.target.classList.contains("effects__radio")) {
+    //imagePreview.removeAttribute("style");
     const effectName = evt.target.value;
     imagePreview.className = `effects__preview--${effectName}`;
-    currentEffect = imageEffect.find((effect) => effect.name === effectName);
-    //updateSlider();
+    imageEffectCurrent = imageEffect.find(
+      (effect) => effect.name === effectName
+    );
+    effectSliderUpdate();
   }
+
+  sliderElement.noUiSlider.on("update", () => {
+    valueElement.value = sliderElement.noUiSlider.get();
+    // filterValueChange(sliderElement.noUiSlider.get());
+    imagePreview.style.filter = `${imageEffectCurrent.filter}(${valueElement.value}${imageEffectCurrent.unit})`;
+  });
 }
+
+//function effectAccept(effectName, filterName) {
+//  imagePreview.className = "";
+//  imagePreview.removeAttribute("style");
+//  imagePreview.classList.add(`${effectName}`);
+//  if (filterName != "none") {
+//    imagePreview.style.filter = `${filterName}(0)`;
+//  }
+//}
