807a0201a985f3ba4d542f3653af897ad90a2f83
Ilia Kovalenko <ilia.kovalenko@pressfire.games>
2023-03-26 22:00:24 +0300

    update after liza

--- js/filter.js
+++ js/filter.js
@@ -1,26 +1,22 @@
-import { imagePreview } from "./scaleImage.js";
-const imgPreviewContainer = document.querySelector(
-  ".img-upload__preview-container"
-);
-const sliderElement = imgPreviewContainer.querySelector(
-  ".effect-level__slider"
-);
-const sliderContainer = imgPreviewContainer.querySelector(
-  ".img-upload__effect-level"
-);
-const valueElement = imgPreviewContainer.querySelector(".effect-level__value");
-//const sliderResult = document.querySelector(".effect-level__value");
+import { imagePreview } from './scale.js';
+
+const imgPreviewContainer = document.querySelector('.img-upload__preview-container');
+const sliderElement = imgPreviewContainer.querySelector('.effect-level__slider');
+const valueElement = imgPreviewContainer.querySelector('.effect-level__value');
+const sliderContainer = imgPreviewContainer.querySelector('.img-upload__effect-level');
+
 const imageEffect = [
-  { name: "none", filter: "none", min: 0, max: 100, step: 1 },
-  { name: "chrome", filter: "grayscale", min: 0, max: 1, step: 0.1, unit: "" },
-  { name: "sepia", filter: "sepia", min: 0, max: 1, step: 0.1, unit: "" },
-  { name: "marvin", filter: "invert", min: 0, max: 100, step: 1, unit: "%" },
-  { name: "phobos", filter: "blur", min: 0, max: 3, step: 0.1, unit: "px" },
-  { name: "heat", filter: "brightness", min: 1, max: 3, step: 0.1, unit: "" },
+  { name: 'none', filter: 'none', min: 0, max: 100, step: 1, unit: '' },
+  { name: 'chrome', filter: 'grayscale', min: 0, max: 1, step: 0.1, unit: '' },
+  { name: 'sepia', filter: 'sepia', min: 0, max: 1, step: 0.1, unit: '' },
+  { name: 'marvin', filter: 'invert', min: 0, max: 100, step: 1, unit: '%' },
+  { name: 'phobos', filter: 'blur', min: 0, max: 3, step: 0.1, unit: 'px' },
+  { name: 'heat', filter: 'brightness', min: 1, max: 3, step: 0.1, unit: '' },
 ];
 
-export const imageEffectNone = imageEffect[0];
-export let imageEffectCurrent = imageEffectNone;
+
+const imageEffectNone = imageEffect[0];
+let imageEffectCurrent = imageEffectNone;
 
 noUiSlider.create(sliderElement, {
   range: {
@@ -29,16 +25,16 @@ noUiSlider.create(sliderElement, {
   },
   start: imageEffectNone.max,
   step: imageEffectNone.step,
-  connect: "lower",
+  connect: 'lower',
 });
 
 const imageEffectOriginalCheck = () => {
   if (imageEffectCurrent === imageEffectNone) {
-    sliderContainer.classList.add("hidden");
-    imagePreview.removeAttribute("class");
-    imagePreview.style.removeProperty("filter");
+    sliderContainer.classList.add('hidden');
+    imagePreview.removeAttribute('class');
+    imagePreview.style.removeProperty('filter');
   } else {
-    sliderContainer.classList.remove("hidden");
+    sliderContainer.classList.remove('hidden');
   }
 };
 
@@ -49,8 +45,8 @@ const effectSliderUpdate = () => {
       min: imageEffectCurrent.min,
       max: imageEffectCurrent.max,
     },
-    step: imageEffectCurrent.step,
     start: imageEffectCurrent.max,
+    step: imageEffectCurrent.step,
   });
 };
 
@@ -60,18 +56,16 @@ export const effectReset = () => {
 };
 
 export function effectChange(evt) {
-  if (evt.target.classList.contains("effects__radio")) {
+  if (evt.target.classList.contains('effects__radio')) {
     const effectName = evt.target.value;
     imagePreview.className = `effects__preview--${effectName}`;
-    imageEffectCurrent = imageEffect.find(
-      (effect) => effect.name === effectName
-    );
+    imageEffectCurrent = imageEffect.find((effect) => effect.name === effectName);
     effectSliderUpdate();
   }
   effectSliderUpdate();
-
-  sliderElement.noUiSlider.on("update", () => {
+  sliderElement.noUiSlider.on('update', () => {
     valueElement.value = sliderElement.noUiSlider.get();
     imagePreview.style.filter = `${imageEffectCurrent.filter}(${valueElement.value}${imageEffectCurrent.unit})`;
   });
 }
+
